88c8fee939c34399477b9b648414ee7b9bce284a
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-03-07 13:19:41 +0000

    Automatically backed up by Learn

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,9 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
-  accepts_nested_attributes_for :user
+  accepts_nested_attributes_for :user, reject_if: :reject_user 
+
+  def reject_user(attributes)
+    attributes['username'].blank?
+  end  
 end
